93d5002f830ab2b6593cfb64d65f57dd2aae8c4e
mounika <mounikamusham12@gmail.com>
2021-12-13 18:06:22 +0530

    string to number

---
+++ string_number.js
@@ -0,0 +1,9 @@
+var str="10";
+console.log(typeof(str));
+console.log(parseInt(str));
+console.log(typeof(parseInt(str)));
+
+var strName="50";
+console.log(typeof(strName));
+console.log(parseInt(strName));
+console.log(typeof(parseInt(strName)));
